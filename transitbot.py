cb9c8e6b3b6e7dd5d360f67a5013939c5a69a914
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-02-02 03:26:07 -0600

    Output param values back to Discord for debugging

--- transitbot.py
+++ transitbot.py
@@ -15,27 +15,33 @@ app = Flask(__name__)
 
 @app.route('/listen', methods=['POST'])
 def hello(name=None):
-    verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+    # verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
 
-    signature = request.headers["X-Signature-Ed25519"]
-    timestamp = request.headers["X-Signature-Timestamp"]
+    # signature = request.headers["X-Signature-Ed25519"]
+    # timestamp = request.headers["X-Signature-Timestamp"]
     body = request.data.decode("utf-8")
 
-    try:
-        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
-    except BadSignatureError:
-        abort(401, 'invalid request signature')
+    # return body
+
+    # try:
+    #     verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+    # except BadSignatureError:
+    #     abort(401, 'invalid request signature')
 
     if request.json["type"] == 1:
         return jsonify({
             "type": 1
         })
     else:
+
+        parsed_body = json.loads(body)
+        parsed_body = parsed_body['data']['options']
+        
         return jsonify({
             "type": 4,
             "data": {
                 "tts": False,
-                "content": "Command received!",
+                "content": json.dumps(parsed_body),
                 "embeds": [],
                 "allowed_mentions": { "parse": [] }
             }
